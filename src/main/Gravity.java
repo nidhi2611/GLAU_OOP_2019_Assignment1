d9b54aa62da9a5ebeed641f3b2e7b9bee88b5cd1
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-18 21:34:13 +0530

    calculations done

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -11,10 +11,14 @@ public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		doublegravity=9.8;
 		double time;
 		double speed;
 		double distance;
 		time=sc.nextDouble();
+		speed=gravity*time;
+		distance=0.5*gravity*time*time;
+
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
